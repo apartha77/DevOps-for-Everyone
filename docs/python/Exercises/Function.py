98516afebf1892a8701b5a8bf745ca37ba584ae4
Partha <45857734+apartha77@users.noreply.github.com>
2022-03-23 12:39:54 +0000

    Create Function.py

---
+++ docs/python/Exercises/Function.py
@@ -0,0 +1,11 @@
+# Function (in-built and custom)
+#----------------------------------------------
+#Inbuilt function random numbers
+import random
+rlist = list()
+for i in range(10):
+    x = random.random()
+    rlist.append(x)
+    #print(x)
+print(rlist)
+#----------------------------------------------
