a48583576c8e695427557da0ee3a4f07f68d9a4a
Partha <45857734+apartha77@users.noreply.github.com>
2022-03-23 12:42:37 +0000

    Create List.py

---
+++ docs/python/Exercises/List.py
@@ -0,0 +1,23 @@
+# Lists
+# # 21-Mar-22 - Traversing a list
+# numlist = [1,2,3,4,5]
+# # To read from List
+# for num in numlist:
+#     print(num)
+# # To write to a List would need index
+# index = 0
+# for i in numlist:
+#     i = i+1
+#     numlist[index] = i
+#     index = index +1
+# print(numlist)
+#---------------------------------------------------------------
+tlist = ['spam', 1, ['Brie', 'Roquefort', 'Pol le Veq'], [1, 2, 3]]
+tnlist = list()
+#tnlist = range(len(tlist))
+trange = range(len(tlist))
+print(trange)
+print(len(tlist))
+
+#print(len(tlist))
+#-------------------------------------------------------------------
